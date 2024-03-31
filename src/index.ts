813eddc93221e60a9fb1eab75d9fb8e06f15ecdd
syhuh <syhuh1125@gmail.com>
2024-03-31 15:03:23 +0900

    19 string type

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,6 @@
-let bigInt1 = BigInt(945845);
+let firstName: string = "Mark";
+let fullName: string = firstName + " " + "Doe";
+fullName.split(" ");
 
-let bigInt2 = 123456545n;
-console.log(bigInt1);
-
-const safeInt = Number.MAX_SAFE_INTEGER;
-console.log(safeInt);
-
-const safeIntPlusOne = safeInt + 1;
-const safeIntPlusTwo = safeInt + 2;
-
-console.log(safeIntPlusOne);
-console.log(safeIntPlusTwo);
-
-console.log(safeIntPlusOne === safeIntPlusTwo);
-
-let a: bigint = BigInt(1234548);
-let b: bigint = 23456465n;
-let c: bigint = a - b;
-
-// let e: bigint = 24551.2n;
-// let f = Math.log(a);
-// console.log(f);
+// fullName = 123456;
+// Math.log(fullName);
--- src/types/any.type.ts
+++ src/types/any.type.ts
@@ -1,3 +1,3 @@
-let firstName: any = "Mark";
-firstName = 123;
-firstName = [5, 4, 8, 4];
+// let firstName: any = "Mark";
+// firstName = 123;
+// firstName = [5, 4, 8, 4];
