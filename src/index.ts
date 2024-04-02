2bfa84a142635f0f2d2469c83bdba035a4570114
syhuh <syhuh1125@gmail.com>
2024-04-02 18:54:59 +0900

    29 Arrarys in TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+const a: number[] = [1, 2, 3];
+
+const b: Array<string> = ["a", "b", "c"];
+
+const c: (number | string)[] = [1, 2, 3, "a", "b"];
+
 type Airplane = {
   flightNumber: string;
   airplaneModel: string;
@@ -10,16 +16,46 @@ type Airplane = {
   };
 };
 
-const airPlane: Airplane = {
-  flightNumber: "SG102",
-  airplaneModel: "A380",
-  dateOfDeparture: "2024/4/5",
-  timeOfDeparture: "10:00 AM",
-  from: "JFA",
-  to: "FRA",
-  seats: {
-    A01: "Frank",
-    A02: "John",
-    A03: "Smith",
+type Airplanes = Airplane[];
+
+const airplanes: Airplanes = [
+  {
+    flightNumber: "SG102",
+    airplaneModel: "A380",
+    dateOfDeparture: "2024/4/5",
+    timeOfDeparture: "10:00 AM",
+    from: "JFA",
+    to: "FRA",
+    seats: {
+      A01: "Frank",
+      A02: "John",
+      A03: "Smith",
+    },
   },
-};
+  {
+    flightNumber: "SG103",
+    airplaneModel: "A380",
+    dateOfDeparture: "2024/4/5",
+    timeOfDeparture: "10:00 AM",
+    from: "JFA",
+    to: "FRA",
+    seats: {
+      A01: "Frank",
+      A02: "John",
+      A03: "Smith",
+    },
+  },
+  {
+    flightNumber: "SG104",
+    airplaneModel: "A380",
+    dateOfDeparture: "2024/4/5",
+    timeOfDeparture: "10:00 AM",
+    from: "JFA",
+    to: "FRA",
+    seats: {
+      A01: "Frank",
+      A02: "John",
+      A03: "Smith",
+    },
+  },
+];
---
+++ src/types/index_signature.ts
@@ -0,0 +1,25 @@
+// type Airplane = {
+//   flightNumber: string;
+//   airplaneModel: string;
+//   dateOfDeparture: string;
+//   timeOfDeparture: string;
+//   from: string;
+//   to: string;
+//   seats: {
+//     [key: string]: string;
+//   };
+// };
+
+// const airPlane: Airplane = {
+//   flightNumber: "SG102",
+//   airplaneModel: "A380",
+//   dateOfDeparture: "2024/4/5",
+//   timeOfDeparture: "10:00 AM",
+//   from: "JFA",
+//   to: "FRA",
+//   seats: {
+//     A01: "Frank",
+//     A02: "John",
+//     A03: "Smith",
+//   },
+// };
