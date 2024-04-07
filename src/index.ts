3788df4ecd7421d6f2730a0cb3633428317c0b5f
syhuh <syhuh1125@gmail.com>
2024-04-07 16:35:19 +0900

    40. Custom Parameters and Return Types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
-function intro(name: string, age: number, contry?: string): string {
-  if (contry) {
-    return `My name is ${name} and age is ${age} I live in ${contry}.`;
-  }
-  return `My name is ${name} and age is ${age}.`;
+enum AgeUnit {
+  years = "years",
+  months = "months",
 }
 
-console.log(intro("John", 32));
-console.log(intro("John", 32, "Canada"));
+type Person = {
+  name: string;
+  age: number;
+  ageUnit: AgeUnit;
+  contry: string;
+};
+
+const person: Person = {
+  name: "Scott",
+  age: 30.5,
+  ageUnit: AgeUnit.years,
+  contry: "USA",
+};
+
+function convertAgeToMonths(person: Person): Person {
+  person.age = person.age * 12;
+  person.ageUnit = AgeUnit.months;
+  return person;
+}
+
+console.log(convertAgeToMonths(person));
