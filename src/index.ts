8ede411f7cdd7fe2b8014836cacc6e2487887e2b
syhuh <syhuh1125@gmail.com>
2024-04-07 16:10:40 +0900

    38. Declring Functions in TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,11 @@
-type TypeOfLibrary = "national" | "public" | "private";
+function intro(name: string, age: number): string {
+  return `My name is ${name} and age is ${age}.`;
+}
 
-type Book = {
-  title: string;
-  pages: number;
-  isbn: string;
+const intro2 = function (name: string, age: number): string {
+  return `My name is ${name} and age is ${age}.`;
 };
 
-type Member = {
-  name: string;
-  phone: string;
-  [key: string]: string;
-};
-
-type Library = {
-  name: string;
-  address: string;
-  numberOfBooks: number;
-  type: TypeOfLibrary;
-  books: Book[];
-  genres: string[];
-  members: member[];
-};
-
-const library: Library = {
-  name: "New York Library",
-  address: "24 Some Street, New York",
-  numberOfBooks: 1254,
-  type: "national",
-  books: [
-    {
-      title: "Harry Potter",
-      pages: 756,
-      isbn: "9971-5-0210-0",
-    },
-    {
-      title: "Davinci Code",
-      pages: 386,
-      isbn: "9971-5-0210-0",
-    },
-  ],
-  genres: ["fiction", "thriller", "mystery"],
-  members: [
-    {
-      name: "John",
-      phone: "123-456-7890",
-      email: "john@gmail.com",
-    },
-    {
-      name: "Smith",
-      phone: "123-456-7890",
-    },
-  ],
+const intro3 = (name: string, age: number): string => {
+  return `My name is ${name} and age is ${age}.`;
 };
---
+++ src/types/solution_strictly_type_a_complex_object.type.ts
@@ -0,0 +1,54 @@
+// type TypeOfLibrary = "national" | "public" | "private";
+
+// type Book = {
+//   title: string;
+//   pages: number;
+//   isbn: string;
+// };
+
+// type Member = {
+//   name: string;
+//   phone: string;
+//   [key: string]: string;
+// };
+
+// type Library = {
+//   name: string;
+//   address: string;
+//   numberOfBooks: number;
+//   type: TypeOfLibrary;
+//   books: Book[];
+//   genres: string[];
+//   members: member[];
+// };
+
+// const library: Library = {
+//   name: "New York Library",
+//   address: "24 Some Street, New York",
+//   numberOfBooks: 1254,
+//   type: "national",
+//   books: [
+//     {
+//       title: "Harry Potter",
+//       pages: 756,
+//       isbn: "9971-5-0210-0",
+//     },
+//     {
+//       title: "Davinci Code",
+//       pages: 386,
+//       isbn: "9971-5-0210-0",
+//     },
+//   ],
+//   genres: ["fiction", "thriller", "mystery"],
+//   members: [
+//     {
+//       name: "John",
+//       phone: "123-456-7890",
+//       email: "john@gmail.com",
+//     },
+//     {
+//       name: "Smith",
+//       phone: "123-456-7890",
+//     },
+//   ],
+// };
