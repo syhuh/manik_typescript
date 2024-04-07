478e825658c49665c75c1b6bae9b0a45c2a0123d
syhuh <syhuh1125@gmail.com>
2024-04-07 16:22:56 +0900

    39. Default and Optional Parameters

---
+++ src/functions/declaring_functions.function.ts
@@ -0,0 +1,11 @@
+// function intro(name: string, age: number): string {
+//   return `My name is ${name} and age is ${age}.`;
+// }
+
+// const intro2 = function (name: string, age: number): string {
+//   return `My name is ${name} and age is ${age}.`;
+// };
+
+// const intro3 = (name: string, age: number): string => {
+//   return `My name is ${name} and age is ${age}.`;
+// };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-function intro(name: string, age: number): string {
+function intro(name: string, age: number, contry?: string): string {
+  if (contry) {
+    return `My name is ${name} and age is ${age} I live in ${contry}.`;
+  }
   return `My name is ${name} and age is ${age}.`;
 }
 
-const intro2 = function (name: string, age: number): string {
-  return `My name is ${name} and age is ${age}.`;
-};
-
-const intro3 = (name: string, age: number): string => {
-  return `My name is ${name} and age is ${age}.`;
-};
+console.log(intro("John", 32));
+console.log(intro("John", 32, "Canada"));
