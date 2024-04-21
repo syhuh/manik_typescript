ee967b0ca91416d57aeea78d25a8c334c9122351
syhuh <syhuh1125@gmail.com>
2024-04-21 21:30:45 +0900

    68. Using a Mutator - Setter Method

---
+++ src/classes_inheritance/getting_more_control_on_members_of_class.classes_inheritance.ts
@@ -0,0 +1,23 @@
+// class Person {
+//   constructor(
+//     private name: string,
+//     public age: number,
+//   ) {
+//     if (age < 0 || age > 200) {
+//       throw new Error("Age must be between 0 and 200");
+//     }
+//   }
+
+//   public getName() {
+//     return this.name;
+//   }
+// }
+
+// const person: Person = new Person("John", 45);
+// const mark: Person = new Person("Mark", 35);
+
+// person.age = 210;
+// mark.age = -10;
+
+// console.log(person);
+// console.log(mark);
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,22 @@
 class Person {
-  constructor(
-    private name: string,
-    public age: number,
-  ) {
-    if (age < 0 || age > 200) {
-      throw new Error("Age must be between 0 and 200");
-    }
-  }
+  private _age: number | undefined;
+
+  constructor(private name: string) {}
 
   public getName() {
     return this.name;
   }
+
+  public set age(age: number) {
+    if (age < 0 || age > 200) {
+      throw new Error("Age must be between 0 and 200");
+    }
+    this._age = age;
+  }
 }
 
-const person: Person = new Person("John", 45);
-const mark: Person = new Person("Mark", 35);
-
-person.age = 210;
-mark.age = -10;
+const person: Person = new Person("John");
+const mark: Person = new Person("Mark");
 
 console.log(person);
 console.log(mark);
